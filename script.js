e4f92604f97291b563b565d5fa75ed051d197d6c
James Glover <james.glover7@outlook.com>
2026-01-08 16:31:46 +0000

    Created a 16 * 16 grid

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let a = 5;
+function createGrid() {
+  let n = 16;
+  const gridContainer = document.querySelector(".grid-container");
+  let flexProperty = "1 1 ";
+  const widthPercentage = (1 / n) * 100;
+  flexProperty += widthPercentage + "%";
+  for (let i = 0; i < n * n; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridItem.style.flex = flexProperty;
+    gridContainer.append(gridItem);
+  }
+}
+
+createGrid();
